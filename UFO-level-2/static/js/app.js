d67ce28fb1b34740be195eab37ae830a151f9513
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2020-12-22 18:05:37 -0500

    created click event with new filter options for data based on datetime, city, state, country and shape input by user

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -19,3 +19,38 @@ tableData.forEach(function(ufoSighting) {
       cell.text(value);
     });
   });
+
+// Select the button - click event
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+
+    tbody.html("");
+
+    // Select the input date, state and shape
+    var inputElement = d3.select("#input");
+    // Retrieve the value property of the input date, state, shape
+    var inputValue = inputElement.property("value");
+    // Filter data with datetime, city, state, country or shape equal to user input
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue ||
+                                                    sighting.city === inputValue ||
+                                                    sighting.state === inputValue ||
+                                                    sighting.country === inputValue ||
+                                                    sighting.shape === inputValue);
+    // console.log filter values
+    console.log(filteredData);
+
+
+    filteredData.forEach(function(selections) {
+
+    console.log(selections);
+    // Append a new table row for each UFO Sighting object
+    var row = tbody.append("tr");
+    // Use `Object.entries` to console.log each UFO Sighting value
+    Object.entries(selections).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+});
