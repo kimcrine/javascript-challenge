dccc75542f161ead626dd16a4dddafb301820100
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2020-12-22 17:32:13 -0500

    added click event to search for datetime input from the user to filter data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -14,8 +14,25 @@ tableData.forEach(function(ufoSighting) {
     // Use 'Object.entries' to console.log each UFO Sighting value
     Object.entries(ufoSighting).forEach(function([key, value]) {
       console.log(key, value);
-      // Append ell to the row for each value
+      // Append cell to the row for each value
       var cell = row.append("td");
       cell.text(value);
     });
   });
+
+// Select button - click event
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+
+    tbody.html("");
+
+    // Select input date
+    var inputElement = d3.select("#datetime");
+    // Retrieve value property of the input date, state, shape
+    var inputValue = inputElement.property("value");
+    // console.log input value
+    console.log(inputValue);
+    // Filter data with datetime equal to input value
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    // console.log filter values
+    console.log(filteredData);
