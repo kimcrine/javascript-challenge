ef96c12e1e1bbe88c8bee789e0cfb520c42c1dca
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2020-12-22 16:49:50 -0500

    created for each function to pull sighting data values and append them into new rows in the table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,20 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Set reference to table body
+var tbody = d3.select("tbody");
+
+// UFO Sighting values for each column
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    // Append one table row 'tr' for each UFO Sighting object
+    var row = tbody.append("tr");
+
+    // Use 'Object.entries' to console.log each UFO Sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append ell to the row for each value
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
